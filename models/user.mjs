314f740847b277b8d2e143e10040769fbfe0a8f3
ibaddkhan <ibadurrehman718@gmail.com>
2024-03-07 22:34:41 +0500

    all issues fixed

--- models/user.mjs
+++ models/user.mjs
@@ -8,6 +8,7 @@ const userSchema = new Schema({
   userName: {
     type: String,
     required: true,
+    unique: true,
   },
   type: {
     type: String,
@@ -16,14 +17,21 @@ const userSchema = new Schema({
   accountnumber: {
     type: Number,
     required: true,
+    unique: true,
   },
   phonenumber: {
     type: Number,
     required: true,
+    unique: true,
   },
   email: {
     type: String,
     required: true,
+    unique: true,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: (props) => `${props.value} is not a valid email address`,
+    },
   },
   password: {
     type: String,
@@ -32,48 +40,39 @@ const userSchema = new Schema({
   },
 });
 
+// Pre-save hook to hash the password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+  }
+  next();
+});
+
 // signup method
-userSchema.statics.signup = async function (userName, email, password, phonenumber, accountnumber, type) {
-  // Check if the email is valid
-  if (!validator.isEmail(email)) {
-    throw Error("Invalid email address");
-  }
-
-  const exists = await this.findOne({ userName });
-  if (exists) {
-    throw Error("User Name already in use.");
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
-
-  const user = await this.create({
-    userName,
-    type,
-    phonenumber,
-    accountnumber,
-    email,
-    password: hash,
-  });
+userSchema.statics.signup = async function (userData) {
+  const user = await this.create(userData);
   return user;
 };
 
 // login method
 userSchema.statics.login = async function (userName, password) {
   if (!userName || !password) {
-    throw Error("User Name and Password are required");
+    throw new Error("User Name and Password are required");
   }
 
   // finding user in the database
   const user = await this.findOne({ userName });
   if (!user) {
-    throw Error("Incorrect user name");
+    throw new Error("Incorrect user name");
   }
 
   // comparing password
   const match = await bcrypt.compare(password, user.password);
   if (!match) {
-    throw Error("Incorrect password");
+    throw new Error("Incorrect password");
   }
   return user;
 };
